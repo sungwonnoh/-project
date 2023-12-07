a173ceae11314f0ed3123a1c2faf287f5c4525e5
jangjanghojae <152234085+jangjanghojae@users.noreply.github.com>
2023-12-07 15:05:33 +0900

    Create 1207장재호 head.h

---
+++ 1207장재호 head.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+using namespace sf;
+
+class Item {
+
+	double x, y, radius;
+	CircleShape circle;
+
+public:
+	Item(double x = 0, double y = 0, double radius = 10, Color fillColor = Color::White) : x{ x }, y{ y }, radius{ radius } {
+		circle.setRadius(radius);
+		circle.setPosition(x, y);
+		circle.setFillColor(fillColor);
+		circle.setPointCount(50);
+
+	}
+
+	void setPosition(double x, double y);
+	void setColor(Color fillColor);
+
+	CircleShape* getCircleShape();
+	bool collisionTest(Item obj);
+};
